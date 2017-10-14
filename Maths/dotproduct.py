c81612226e1cf9848554442e224472ab3730815b
Mohammad <momosharif58@gmail.com>
2017-10-13 21:03:29 -0700

    Updated dotproduct to use lambda functions with map and reduce

--- Maths/dotproduct.py
+++ Maths/dotproduct.py
@@ -2,14 +2,15 @@
 The following is an implementation that takes to python lists and returns the
 dot product. If the lists are of different lengths or types, an error is printed.
 """
+from functools import reduce
 
 def dot_product(first_list, second_list):
-    dot_product = 0
     if(len(first_list) == len(second_list)):
         for i in range(0, len(first_list)):
             try:
                 # multiply the two list items, and add them to the current total
-                dot_product += (first_list[i] * second_list[i])
+                # We use zip to interlace the two lists into one, then we run lambda functions to multipy each value together and then add
+                dot_product = reduce(lambda y1, y2: y1 + y2, map(lambda x: reduce(lambda x1, x2: x1*x2, x), zip(first_list, second_list)))
             except:
                 print("One of your lists has an invalid type")
                 return
