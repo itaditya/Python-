d5c618b944e9c63912f1f6a72bd6382698f6f7af
Mohammad <momosharif58@gmail.com>
2017-10-17 11:14:07 -0700

    Made requested changes, removed for loop and added return statement

--- Maths/dotproduct.py
+++ Maths/dotproduct.py
@@ -6,14 +6,13 @@ from functools import reduce
 
 def dot_product(first_list, second_list):
     if(len(first_list) == len(second_list)):
-        for i in range(0, len(first_list)):
-            try:
-                # multiply the two list items, and add them to the current total
-                # We use zip to interlace the two lists into one, then we run lambda functions to multipy each value together and then add
-                dot_product = reduce(lambda y1, y2: y1 + y2, map(lambda x: reduce(lambda x1, x2: x1*x2, x), zip(first_list, second_list)))
-            except:
-                print("One of your lists has an invalid type")
-                return
-        print(dot_product)
+        try:
+            # multiply the two list items, and add them to the current total
+            # We use zip to interlace the two lists into one, then we run lambda functions to multipy each value together and then add
+            dot_product = reduce(lambda y1, y2: y1 + y2, map(lambda x: reduce(lambda x1, x2: x1*x2, x), zip(first_list, second_list)))
+        except:
+            print("One of your lists has an invalid type")
+            return
+        return dot_product
     else:
         print("The lists are not of the same length")
